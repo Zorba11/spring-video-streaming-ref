c4add299779ce5bbfc62e382aab4a0f834cb96a7
Zorba11 <zorbathegreek111@gmail.com>
2021-09-18 12:21:51 -0600

    done !

---
+++ src/main/java/com/zorba11/videostreamingwebflux/VideoStreamingWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.zorba11.videostreamingwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamingWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zorba11/videostreamingwebflux/VideoStreamingWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorba11.videostreamingwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
